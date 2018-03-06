efb2a71ae626e69eb9b707b806b78f00d3355876
franek <franek.szombara@gmail.com>
2018-03-06 22:14:54 +0100

    win message changed

--- js/app.js
+++ js/app.js
@@ -42,11 +42,41 @@ function match(array){
 
 //checks if the game is won, displays end game alert
 function won(){
+
 	if (score === 8){
-		const starRating = "* ".repeat(stars)
-		if (confirm(`star rating: ${starRating} You won in ${cnt} moves and it took you ${time} seconds\n play again?`)){
-			location.reload()
+		stop = true;
+
+		time = (Date.now() - start )/1000;
+
+		for (let i = 0; i < cards.length; i++){
+			document.getElementsByClassName("card")[0].remove();
 		}
+
+		const starRating = "* ".repeat(stars);
+
+
+		const deck = document.getElementsByClassName("deck")[0];
+
+		let win_div = document.createElement("div");
+		win_div.setAttribute("class", "win_message");
+
+		let win_message = document.createElement("p");
+		const ms1 = `star rating: ${starRating}`
+		const ms2 = `You won in ${cnt} moves and it took you ${time} seconds`
+		const ms3 = "play again?"
+		win_message.innerHTML = ms1+"<br/>"+ms2+"<br/>"+ms3
+
+		let win_button = document.createElement("button");
+		win_button.setAttribute("id", "win_button");
+		win_button.textContent = "Yes!";
+
+		deck.appendChild(win_div);
+		win_div.appendChild(win_message);
+		win_div.appendChild(win_button);
+
+		document.getElementById("win_button").addEventListener("click", function(){
+			location.reload()
+		})
 }}
 
 //takes an array of 2 cards, runs animation andhides the icons of cards
@@ -65,21 +95,26 @@ function hide(array){
 
 //Increases the move count and sets the star rating, starts the clock
 function increase_cnt(){
+
 	if (cnt === 0){
 		start = Date.now();
 		update_clock();
 	}
-
-	cnt = Number(document.getElementById("cnt").textContent);
-	cnt +=1;
-	document.getElementById("cnt").textContent = cnt;
-	if (cnt > 22 && cnt < 30){
-		document.getElementById("star1").style.visibility = "hidden";
-		stars = 2;
-	}
-	if (cnt > 30){
-		stars = 1;
-		document.getElementById("star2").style.visibility = "hidden";
+	if (on === true){
+		on = false;
+		cnt = Number(document.getElementById("cnt").textContent);
+		cnt +=1;
+		document.getElementById("cnt").textContent = cnt;
+		if (cnt > 12 && cnt < 16){
+			document.getElementById("star1").style.visibility = "hidden";
+			stars = 2;
+		}
+		if (cnt > 16){
+			stars = 1;
+			document.getElementById("star2").style.visibility = "hidden";
+		}
+	} else {
+		on = true;
 	}
 }
 
@@ -87,7 +122,7 @@ function increase_cnt(){
 //showes the clicked card and checks if two cards are of the same type
 function clicked(e){
 	increase_cnt();
-	time = (Date.now() - start )/1000
+	
     e.target.classList.toggle('open');
     e.target.classList.toggle('show');
     e.target.classList.toggle("disabled");
@@ -109,8 +144,10 @@ function clicked(e){
 
 //updates the content of clock element
 function update_clock(){
-	document.getElementById("clock").textContent = (Date.now() - start )/1000;
-	setTimeout(update_clock, 100);
+	if (stop === false){
+		document.getElementById("clock").textContent = (Date.now() - start )/1000;
+		setTimeout(update_clock, 50);
+	}
 }
 
 
@@ -124,8 +161,10 @@ let card = "";
 let open = [];
 let start = 0;
 let time = 0;
-score = 0;
+let score = 0;
+let on = false;
 let cnt = Number(document.getElementById("cnt").textContent);
+let stop = false;
 
 //set a click event at each card
 
